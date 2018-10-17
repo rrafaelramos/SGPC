231846412ca91481a194d14215e878011aff74ae
marcoalvesalmeida <marco.ifnmg@gmail.com>
2018-10-17 10:08:21 -0300

    CRUD Funcionario

--- ci/application/controllers/Login.php
+++ ci/application/controllers/Login.php
@@ -1,35 +1,48 @@
-public function login()
-{
-  // Carrega os dados recebidos por POST
-  $func['login'] = $this->input->post('login');
-  $func['senha'] = $this->input->post('senha');
-  // Carrega o model
-  $this->load->model(Funcionarios_Model, 'funcionario');
-  // Recebe os dados do model
-  if($funcionario->login($func)){
-    $this->session->set_userdata(array("login"=>$func['login'], "logado"=>true));
-  }else{
-    echo "Erro ao logar!";
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+  public function login()
+  {
+    // Carrega os dados recebidos por POST
+    $func['login'] = $this->input->post('login');
+    $func['senha'] = $this->input->post('senha');
+    // Carrega o model
+    $this->load->model(Funcionarios_Model, 'funcionario');
+    // Recebe os dados do model
+    if($funcionario->login($func)){
+      $this->session->set_userdata(array("login"=>$func['login'], "logado"=>true));
+    }else{
+      echo "Erro ao logar!";
+    }
+
+    //$this->load->view('welcome_message');
   }
 
-  //$this->load->view('welcome_message');
-}
+  public function verify_login()
+  {
+    if($this->session->userdata('logado')){
+      echo "Válido! Usuário logado!";
+    }else{
+      echo "Erro ao logar!";
+    }
 
-public function verify_login()
-{
-  if($this->session->userdata('logado')){
-    echo "Válido! Usuário logado!";
-  }else{
-    echo "Erro ao logar!";
+    //$this->load->view('welcome_message');
   }
 
-  //$this->load->view('welcome_message');
-}
+  public function logout()
+  {
+      $this->session->set_userdata('login', '');
+      $this->session->set_userdata('logado', false);
+      // destroy session
+      $this->session->sess_destroy();
+  }
 
-public function logout()
-{
-    $this->session->set_userdata('login', '');
-    $this->session->set_userdata('logado', false);
-    // destroy session
-    $this->session->sess_destroy();
 }

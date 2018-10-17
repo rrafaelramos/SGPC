f53b2a7cea78805a082042891addb051706911c5
marcoalvesalmeida <marco.ifnmg@gmail.com>
2018-10-17 10:01:24 -0300

    CRUD Funcionario

---
+++ ci/application/controllers/Login.php
@@ -0,0 +1,35 @@
+public function login()
+{
+  // Carrega os dados recebidos por POST
+  $func['login'] = $this->input->post('login');
+  $func['senha'] = $this->input->post('senha');
+  // Carrega o model
+  $this->load->model(Funcionarios_Model, 'funcionario');
+  // Recebe os dados do model
+  if($funcionario->login($func)){
+    $this->session->set_userdata(array("login"=>$func['login'], "logado"=>true));
+  }else{
+    echo "Erro ao logar!";
+  }
+
+  //$this->load->view('welcome_message');
+}
+
+public function verify_login()
+{
+  if($this->session->userdata('logado')){
+    echo "Válido! Usuário logado!";
+  }else{
+    echo "Erro ao logar!";
+  }
+
+  //$this->load->view('welcome_message');
+}
+
+public function logout()
+{
+    $this->session->set_userdata('login', '');
+    $this->session->set_userdata('logado', false);
+    // destroy session
+    $this->session->sess_destroy();
+}

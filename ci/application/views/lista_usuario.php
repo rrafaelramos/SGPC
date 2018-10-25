43ddc21829ab4599156f07ad9a2d157c9ada2d62
João Victor Melo <victormelocr2@outlook.com>
2018-10-25 09:31:49 -0200

    Adição da tela Listar

---
+++ ci/application/views/lista_usuario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Usuários</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+  </head>
+  <body>
+    <h2 align="center"> Usuários </h2>
+    <div class="container col-md-offset-3 col-md-6" style="border: solid 1px #d5d5d5; border-radius: 5px; background-color: white; padding: 15px;">
+
+    </div>
+  </body>
+</html>
